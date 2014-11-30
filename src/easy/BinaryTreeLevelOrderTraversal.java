f4948cce6f0834f9c2f7da7d85144b689695f023
genustin <wqhuang@me.com>
2014-11-30 21:13:31 +0800

    breath-first traversal.

---
+++ src/easy/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,59 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Created by Genustin on 11/30/14.
+ * <p/>
+ * Given a binary tree, return the level order traversal of its nodes' values.
+ * (ie, from left to right, level by level).
+ * <p/>
+ * case 1: root == null, return an empty list. ie, []
+ */
+public class BinaryTreeLevelOrderTraversal {
+    // Definition for binary tree
+    private class TreeNode {
+        private int      val;
+        private TreeNode left;
+        private TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> levels = new ArrayList<List<Integer>>();
+        // setup two queues to handle this level and next level
+        // if aqueue is not empty, visit each element, put children to bqueue,
+        // and vice versa.
+        Queue<TreeNode> aqueue = new LinkedList<TreeNode>();
+        Queue<TreeNode> bqueue = new LinkedList<TreeNode>();
+        // start from aqueue
+        if (null != root) aqueue.add(root);
+        while (!aqueue.isEmpty() || !bqueue.isEmpty()) {
+            if (!aqueue.isEmpty())
+                levels.add(visit(aqueue, bqueue));
+            else
+                levels.add(visit(bqueue, aqueue));
+        }
+        return levels;
+    }
+
+    private List<Integer> visit(Queue<TreeNode> level, Queue<TreeNode> child) {
+        List<Integer> list = new ArrayList<Integer>();
+        TreeNode node;
+        while (!level.isEmpty()) {
+            node = level.poll();
+            list.add(node.val);
+            if (null != node.left)
+                child.add(node.left);
+            if (null != node.right)
+                child.add(node.right);
+        }
+        return list;
+    }
+}
