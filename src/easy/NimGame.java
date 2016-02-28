5ec06576c62946abf4901fc8d7f3049c5505031e
genustin <wqhuang@me.com>
2016-02-28 23:58:32 +0800

    nim game, for 1 heap

---
+++ src/easy/NimGame.java
@@ -0,0 +1,16 @@
+package easy;
+
+/**
+ * Created by Genustin on 2/28/16.
+ * <p/>
+ * 2 people play 1 heap of stone, player has to take 1~3 stones each turn
+ * who take the last stone is the winner,
+ * start from you, would you win given n stone here?
+ *
+ * what if more heaps?
+ */
+public class NimGame {
+    public boolean canWinNim(int n) {
+        return n % 4 > 0;
+    }
+}
