c5c8fabb31322f55cbda47d4a4671e973d946948
genustin <wqhuang@me.com>
2016-04-11 00:23:13 +0800

    add more solution

---
+++ src/easy/ReverseList.java
@@ -0,0 +1,29 @@
+package easy;
+
+/**
+ * Created by Genustin on 4/11/16.
+ */
+public class ReverseList {
+
+    class ListNode {
+        int      val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null) return head;
+        ListNode node, prev = null;
+        while (head.next != null) {
+            node = head.next;
+            head.next = prev;
+            prev = head;
+            head = node;
+        }
+        head.next = prev;
+        return head;
+    }
+}
