34176c7d567e99af9fea39962bb66cadaa9e9b4a
genustin <wqhuang@me.com>
2016-08-07 22:04:56 +0800

    bit opertion to sum to number

--- src/com/genustin/easy/AddDigits.java
+++ src/com/genustin/easy/AddDigits.java
@@ -3,13 +3,13 @@ package com.genustin.easy;
 /**
  * Created by wayne on 16/2/29.
  * Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
- *
+ * <p>
  * For example:
  * Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
- *
+ * <p>
  * Could you do it without any loop/recursion in O(1) runtime?
  */
 public class AddDigits {
 
-// TODO: 16/3/17  
+    // TODO: 16/3/17
 }
---
+++ src/com/genustin/easy/SumTwoInteger.java
@@ -0,0 +1,43 @@
+package com.genustin.easy;
+
+/**
+ * Created by Genustin on 8/7/16.
+ * <p>
+ * Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+ * <p>
+ * Example:
+ * Given a = 1 and b = 2, return 3.
+ */
+public class SumTwoInteger {
+    /**
+     * bit operation,
+     * a & b, identify 1 vs 1 cases, which will be the carry
+     * a ^ b, identify 1 vs 0 cases, which will be the residue
+     * note the number is saved as 补码
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public int getSum(int a, int b) {
+        // carry bits
+        int carry = (a & b) << 1;
+        // residue bits
+        int residue = a ^ b;
+//        System.out.println("carry:" + carry);
+//        System.out.println("residue:" + residue);
+
+        // note the sign number
+        if (carry != 0) {
+            return getSum(carry, residue);
+        }
+
+        return residue;
+    }
+
+    public static void main(String[] args) {
+        SumTwoInteger sti = new SumTwoInteger();
+        int z = sti.getSum(1, -2);
+        System.out.println(z);
+    }
+}
