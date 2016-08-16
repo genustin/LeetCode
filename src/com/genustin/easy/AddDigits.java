c1e6ce1619eb3917235d6bb6ca162052f46fe7e3
wqhuang <huangwenqiang@didichuxing.com>
2016-08-16 10:50:10 +0800

    two more solutions

--- src/com/genustin/easy/AddDigits.java
+++ src/com/genustin/easy/AddDigits.java
@@ -8,8 +8,24 @@ package com.genustin.easy;
  * Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
  * <p>
  * Could you do it without any loop/recursion in O(1) runtime?
+ *
+ * https://en.wikipedia.org/wiki/Digital_root
  */
 public class AddDigits {
+    public int addDigits(int num) {
+        int dr = num;
+        while(dr > 9)
+            dr = dr - ((dr - 1) / 9) * 9;
+        return dr;
+    }
 
-    // TODO: 16/3/17
+    public static void main(String[] args) {
+        AddDigits ad = new AddDigits();
+        System.out.println(ad.addDigits(11));
+        System.out.println(ad.addDigits(12));
+        System.out.println(ad.addDigits(38));
+        System.out.println(ad.addDigits(1853));
+        System.out.println(ad.addDigits(65535));
+    }
 }
+
---
+++ src/com/genustin/easy/UglyNumber.java
@@ -0,0 +1,43 @@
+package com.genustin.easy;
+
+/**
+ * Created by wayne on 16/8/16.
+ * <p>
+ * Ugly numbers are positive numbers whose prime factors only include 2, 3, 5.
+ * For example, 6, 8 are ugly while 14 is not ugly since it includes another prime factor 7.
+ * <p>
+ * Note that 1 is typically treated as an ugly number.
+ */
+public class UglyNumber {
+    public boolean isUgly(int num) {
+        if (num <= 0) return false;
+        int number = num;
+        while (number % 2 == 0)
+            number = number >> 1;
+        while (number % 3 == 0)
+            number = number / 3;
+        while (number % 5 == 0)
+            number = number / 5;
+
+        return number == 1;
+    }
+
+    public static void main(String[] args) {
+        UglyNumber un = new UglyNumber();
+        System.out.println(un.isUgly(-1));
+        System.out.println(un.isUgly(0));
+        System.out.println(un.isUgly(1));
+        System.out.println(un.isUgly(2));
+        System.out.println(un.isUgly(3));
+        System.out.println(un.isUgly(4));
+        System.out.println(un.isUgly(5));
+        System.out.println(un.isUgly(6));
+        System.out.println(un.isUgly(7));
+        System.out.println(un.isUgly(8));
+        System.out.println(un.isUgly(9));
+        System.out.println(un.isUgly(10));
+        System.out.println(un.isUgly(11));
+        System.out.println(un.isUgly(14));
+        System.out.println(un.isUgly(2147483647));
+    }
+}
