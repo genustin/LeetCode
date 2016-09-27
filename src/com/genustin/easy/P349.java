420970bf27d2c1e84c83ca4f3ec8eba17a649e75
wqhuang <huangwenqiang@didichuxing.com>
2016-09-27 11:43:45 +0800

    refine

--- src/com/genustin/easy/P349.java
+++ src/com/genustin/easy/P349.java
@@ -1,6 +1,7 @@
 package com.genustin.easy;
 
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
@@ -16,21 +17,24 @@ public class P349 {
     public int[] intersection(int[] nums1, int[] nums2) {
         Set<Integer> hello = new HashSet<>();
         Set<Integer> inter = new HashSet<>();
+        if (nums1.length < nums2.length) {
+            int[] tmp = nums1;
+            nums1 = nums2;
+            nums2 = tmp;
+        }
 
         for (int i = 0; i < nums1.length; i++) {
             hello.add(nums1[i]);
         }
         for (int i = 0; i < nums2.length; i++) {
-            if (hello.contains(nums2[i]))
+            if (hello.contains(nums2[i]) && !inter.contains(nums2[i]))
                 inter.add(nums2[i]);
         }
         int[] sol = new int[inter.size()];
         Iterator<Integer> x = inter.iterator();
         int i = 0;
-        while (x.hasNext()) {
-            sol[i] = x.next();
-            i++;
-        }
+        while (x.hasNext())
+            sol[i++] = x.next();
         return sol;
     }
 
